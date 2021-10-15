eb3dc50b7f8ad55ed1f519dee62fcc9445b025a2
Noah Weaver <noahweaver@aol.com>
2021-10-15 14:21:23 -0500

    small changes to business time and CSS grid from mid-level assessment. js practice is arrays and loops performed in node.

--- business-time/business-time.js
+++ business-time/business-time.js
@@ -1,22 +1,21 @@
 var homeButton = document.getElementById("home-button");
-    console.log("homeButton");
-    homeButton.addEventListener("click", function(){
-        document.location.href = "http://127.0.0.1:5500/assignments/business-time/business-time-home.html"
+console.log("homeButton");
+homeButton.addEventListener("click", function(){
+ document.location.href = "http://127.0.0.1:5500/assignments/business-time/business-time-home.html"       
     });
 var aboutButton = document.getElementById("about-button");
-    console.log("aboutButton");
-    aboutButton.addEventListener("click", function(){
-        document.location.href = "http://127.0.0.1:5500/assignments/business-time/business-time-about.html"
-    });
+console.log("aboutButton");
+aboutButton.addEventListener("click", function(){
+    document.location.href = "http://127.0.0.1:5500/assignments/business-time/business-time-about.html"
+});
 var contactButton = document.getElementById("contact-button");
-    console.log("contactButton");
-    contactButton.addEventListener("click", function(){
-        document.location.href = "http://127.0.0.1:5500/assignments/business-time/business-time-contact.html"
-    });
+console.log("contactButton");
+contactButton.addEventListener("click", function(){
+    document.location.href = "http://127.0.0.1:5500/assignments/business-time/business-time-contact.html"
+});
 
-    // Contact Page
-function sendEmail()
-{
+// Contact Page
+function sendEmail(){
     var mailto = "noahweaver@aol.com";
     var body = "Dear DaWeavs Development,";
     var subject = "Business Inquiry";
---
+++ js-practice/practice.js
@@ -0,0 +1,38 @@
+//#1
+// var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+// for (var i = 0; i < officeItems.length; i++){
+//     if (officeItems[i] === "computer"){
+//         console.log(officeItems[i])
+// }}
+//#2
+// var peopleWhoWantToSeeMadMaxFuryRoad = [
+//     {
+//       name: "Mike",
+//       age: 12,
+//       gender: "male"
+//     },{
+//       name: "Madeline",
+//       age: 80,
+//       gender: "female"
+//     },{
+//       name: "Cheryl",
+//       age: 22,
+//       gender: "female"
+//     },{
+//       name: "Sam",
+//       age: 30,
+//       gender: "male"
+//     },{
+//       name: "Suzy",
+//       age: 4,
+//       gender: "female"
+//     }
+//   ]
+  
+//   for (var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+//       if(peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18) {
+//           console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough")}
+//       if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+//           console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough")}
+//   }
+//#3
