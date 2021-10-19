4c18cd88d0538289274e48f2d1c10202944d072d
Noah Weaver <noahweaver@aol.com>
2021-10-19 10:11:02 -0500

    finished the employee-records assignment. conosle is still printing the print function (anonymous)...still need to fix this

--- js-practice/employee-records.js
+++ js-practice/employee-records.js
@@ -1,23 +1,25 @@
 //make fulltime the default
 
-employees = []
-function Employee(name, title, salary, status){
+var employees = []
+function Employee(name, title, salary, status = "Full-Time"){
     this.name = name
     this.title = title
     this.salary = salary
-    this.status = "Full Time"
+    this.status = status
     this.print = function(){
         console.log(this)
     }
+    employees.push(this.name)
     }    
 
-var bill = new Employee ("Bill", "Program Manager", "$90k", "contract")
-var emily = new Employee("Emily", "CEO", "$1.6M", "full time")
+var bill = new Employee ("Bill", "Program Manager", "$90k", "Contract")
+var emily = new Employee("Emily", "CEO", "$1.6M")
 var john = new Employee("John", "Warehouse Operator", "$15/hr", "Part-Time" )
 
 bill.print()
 emily.print()
 john.print()
+console.log(employees)
 
 // Employee.prototype.print = function(){
 //     console.log(this.Employee)
