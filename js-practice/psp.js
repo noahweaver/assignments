8edf0951b8ea9e4e64859e7d9a2fa497bcb9fd8d
Noah Weaver <noahweaver@aol.com>
2021-10-20 09:41:58 -0500

    completed problem solving practice assignment. completed function that returns all words in an array containing a certain character and function that checks if num1 is divisible by num2

--- js-practice/psp.js
+++ js-practice/psp.js
@@ -1,5 +1,12 @@
 //Problem Solving Practice
-//return the largest number in an array
+//4 steps to problem solving
+//1 Fully understand the problem
+//2 Plan out a solution
+//3 Carry out your plan
+//4 Review your solution
+
+
+//part 1: return the largest number in an array
 function largestNum (numbers){
     let largest = numbers[0];
     for (let i = 0; i < numbers.length; i++){
@@ -9,33 +16,31 @@ function largestNum (numbers){
     }
     console.log(largest);
 }
-largestNum([1, 5, 7, 3])
-//attempt 2
-// function largest(numbers) {  
-//     let max = numbers[0]; 
-//     for (let i = 0; i < numbers.length; i++) {
-//         if (numbers[i] > max) 
-//             max = numbers[i]; 
-//     } 
-//     console.log(max);
-// } 
-// largest([3, 5, 6, 75, 23, 48])
+largestNum([1, 5, 7, 3]) // => 7
 
-
-//return all words in an array containing a certain character
-function lettersWithStrings (lettersArr, strings){
+//part 2: return all words in an array containing a certain character
+function lettersWithStrings (lettersArr, b ){
     let words = lettersArr[0];
     for (let i = 0; i < lettersArr.length; i++){
-        if (lettersArr[i].includes("!")){
+        if (lettersArr[i].includes(b)){
             words = lettersArr[i]
             console.log(words)
         }
     }
- //still need to log the string at the end
-    let includedStrings = ""
-    if (includedStrings.includes("!")){
-        includedStrings = strings
-        console.log(strings)
- }
 }
-lettersWithStrings(["#3", "C$$$", "C%4!", "Hey!"], "!") 
+lettersWithStrings(["#3", "C$$$", "C%4!", "Hey!"], "!") //=> C%4! Hey!
+
+//part 3: write a function that takes num1 and num2 and returns whether num1 is divisible by num 2
+let isDivisible = false;
+function divisibleBy (num1, num2){
+    if (num1 % num2 === 0) {
+        isDivisible = true;
+        console.log(isDivisible)
+    } else {
+        isDivisible = false;
+        console.log(isDivisible)
+    }
+}
+divisibleBy(25, 5) // => true
+divisibleBy(15, 4) // => false
+divisibleBy(56, 8) // => true
