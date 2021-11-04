5846661f157f6276ffbee091367a6c62aeae7c54
Noah Weaver <noahweaver@aol.com>
2021-11-04 10:25:51 -0500

    Pokemon List HTTP assignment complete

--- level-2/http/pokemon.js
+++ level-2/http/pokemon.js
@@ -11,14 +11,14 @@ xhr.onreadystatechange = function(){
         if(xhr.readyState === 4 && xhr.status === 200){
             const JSONdata = xhr.responseText
             const data = JSON.parse(JSONdata)
-            pokemonList(data.objects)
-            console.log(data.objects)
+            pokemonList(data.objects[0].pokemon)
+            console.log(data.objects[0].pokemon)
         } 
     }
-function pokemonList(pokemon){
-        for (let i = 0; i < pokemon.length; i++) {
-                const h3 = document.createElement('h3')
-                h3.textContent = pokemon[i].name
-                document.body.appendChild(h3)
+function pokemonList(arr){
+        for (let i = 0; i < arr.length; i++) {
+        const h3 = document.createElement('h3')
+        h3.textContent = arr[i].name
+        document.body.appendChild(h3)
         }
 }
