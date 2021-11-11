15fdc263cd510ded3439b4c6f45357e6cc8bfd2a
Noah Weaver <noahweaver@aol.com>
2021-11-11 11:29:53 -0600

    completed name entry assignment

--- level-3/name-app/src/App.js
+++ level-3/name-app/src/App.js
@@ -1,11 +1,65 @@
 import React from 'react'
 
-
-class App extends Component {
+class App extends React.Component {
     constructor(){
         super()
         this.state = {
-            names: []
+            name: "",
+            namesArr: []
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.addName = this.addName.bind(this)
+
+    }
+    addName(){
+        const list = document.getElementById("name-list")
+        while(list.firstChild){
+            list.removeChild(list.firstChild)
+        }
+
+        this.setState(prevState => {
+            console.log("addName")
+            prevState.namesArr.push(this.state.name)
+            console.log(this.state.namesArr)
+            prevState.namesArr.map( name => {
+                const listItem = document.createElement("li")
+                document.getElementById("name-list").appendChild(listItem).textContent = name
+            })
+        })
+        //clear input
+        document.getElementById("inputName").value = ""
+
+    }
+    handleChange(event){
+        console.log("handleChange")
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+    render(){
+        return(
+            <div>
+                <input
+                    id="inputName"
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+
+                    />
+                <button onClick={this.addName}>
+                    Add to list
+                </button>
+
+                <h1>{this.state.name}</h1>
+                <ul id="name-list">
+                    {/* Added names will appear here */}
+                </ul>
+            </div>
+        )
     }
 }
+
+
+export default App
