d2b8d7bd53222c91fc5feb8027af31238c41fe0f
Noah Weaver <noahweaver@aol.com>
2021-11-11 18:53:12 -0600

    create and worked on name badge app. I can return all state values but not in the way I want them to appear

---
+++ level-3/name-badge-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Form from "./components/Form"
+import "./Styles.css"
+
+
+function App () {
+    return(
+        <div>
+            <Form />
+        </div>
+    )
+    }
+
+export default App
---
+++ level-3/name-badge-app/src/components/Form.js
@@ -0,0 +1,163 @@
+import React from 'react'
+
+class Form extends React.Component{
+    
+    
+    constructor(){
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            placeOfBirth: "",
+            favFood: "",
+            email: "",
+            phone: 0,
+            addComment: "",
+            badgesArr: []
+        }
+        this.submitBadge = this.submitBadge.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event){
+        event.preventDefault()
+        console.log("handleChange")
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+    // handleChange(event) {
+    //     this.setState({value: event.target.value});
+    //   }
+    submitBadge(event){
+        event.preventDefault()
+        const list = document.getElementById("badge-list")
+        while(list.firstChild){
+            list.removeChild(list.firstChild)
+        }
+
+        this.setState(prevState => {
+            console.log("submitBadge")
+            prevState.badgesArr.push(this.state)
+            console.log(this.state.badgesArr)
+            prevState.badgesArr.map( badge => {
+                const listItem = document.createElement("li")
+                document.getElementById("badge-list").appendChild(listItem).textContent = Object.values(badge)
+                console.log(badge)
+                return
+                    
+            })
+        })
+        document.getElementById("firstName").value = ""
+        document.getElementById("lastName").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("birthplace").value = ""
+        document.getElementById("phone").value = ""
+        document.getElementById("favFood").value = ""
+        document.getElementById("commentArea").value = ""
+    }
+    render(){
+        
+        const formStyles = {
+            border: "2px solid black",
+            borderRadius: "2%",
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            margin: "5% 25% 5% 25%",
+            padding: "5% 25% 5% 25%"
+            //need to center boxes
+        }
+        const textAreaStyle = {
+            gridColumnStart: 1,
+            gridColumnEnd: 3,
+            gridRow: (4 , 5),
+            width: "95%"
+        }
+        const buttonStyle = {
+            gridColumnStart: 1,
+            gridColumnEnd: 3,
+            width: "50%",
+            gridRow: (5 , 6)    
+            //center button on inputs
+            
+        }
+
+        return(
+            <div>
+                <div style={formStyles} className="form-container">
+                    <input 
+                    id="firstName"
+                    name="firstName"
+                    placeholder="First Name" 
+                    minLength="3"
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
+                    required
+                    />
+                    <input 
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Last Name"
+                    minLength="3"
+                    value={this.state.lastName}
+                    onChange={this.handleChange}
+                    required
+                    />
+                    <input 
+                    id="email"
+                    name="email"
+                    placeholder="E-mail"
+                    minLength="3"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    required
+                    />
+                    <input 
+                    id="birthplace"
+                    name="placeOfBirth"
+                    placeholder="Place of Birth"
+                    minLength="3"
+                    value={this.state.placeOfBirth}
+                    onChange={this.handleChange}
+                    required
+                    />
+                    <input 
+                    //needs to take numbers only
+                    id="phone"
+                    name="phone"
+                    placeholder="Phone Number"
+                    type="phone"
+                    minLength="3"
+                    maxLength="11"
+                    value={this.state.phone}
+                    onChange={this.handleChange}
+                    required
+                    />
+                    <input 
+                    id="favFood"
+                    name="favFood"
+                    placeholder="Favorite Food"
+                    minLength="3"
+                    value={this.state.favFood}
+                    onChange={this.handleChange}
+                    required
+                    />
+                    <textarea 
+                    id="commentArea"
+                    name="addComment"
+                    value={this.state.addComment}
+                    onChange={this.handleChange}
+                    placeholder="Important information or additional comments"
+                    style={textAreaStyle}
+                    />
+                    <button style={buttonStyle} onClick={this.submitBadge}>Submit</button>
+                </div>
+                <ul id="badge-list">
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Form
