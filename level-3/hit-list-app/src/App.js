4bf107ba8c4aa829093785e0fd8556d3b35fca0d
Noah Weaver <noahweaver@aol.com>
2021-11-19 22:25:13 -0600

    converted to use hooks

--- level-3/hit-list-app/src/App.js
+++ level-3/hit-list-app/src/App.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Target from './Target'
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            hitList: []
-        }
-    }
-    componentDidMount(){
-        console.log("DidMount")
+function App () {
+
+    const [hitList, setList] = useState([])
+    
+    useEffect(() => {
+        console.log("useEffect DidMount")
         fetch("https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json")
             .then(response => response.json())
-            .then(data => {this.setState({
-                hitList: data
-                })
-                console.log(this.state.hitList)
-            })
-            
-    }
-
-    render() {
-        console.log("render")
-        const targets = this.state.hitList.map((target, index) => (<Target key={index} target={target}/>))
-        return (
+            .then(data => {
+                setList(data)
+            }) 
+        }, [])
+    
+    const targets = hitList.map((target, index) => (<Target key={index} target={target}/>))
+    
+    return (
             <div>
                 {targets}
             </div>
         )
+
     }
-}
+
 
 export default App
