031336e59ab27f4c0d0a1e6efd45a99375a3194c
Noah Weaver <noahweaver@aol.com>
2021-11-29 17:24:53 -0600

    footer styled, still needs github icon link

--- level-4/lvl4-cap-app/src/reused_components/Footer.js
+++ level-4/lvl4-cap-app/src/reused_components/Footer.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-            <h1>FOOTER</h1>
-        </div>
+        <footer className="fixed-bottom flex-shrink-0 py-4 bg-dark text-white-50">
+            {/* make footer sticky */}
+            <div className="container text-center">
+                <p>JokesApp</p>
+                {/* link to github repo */}
+            </div>
+        </footer>
     )
 }
 
