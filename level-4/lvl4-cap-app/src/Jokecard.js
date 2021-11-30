2d8f2d2a832e7a41bfcfc6627d7a1a8195684b03
Noah Weaver <noahweaver@aol.com>
2021-11-30 14:12:25 -0600

    styled card, still needs conditional rendering based on active route

--- level-4/lvl4-cap-app/src/Jokecard.js
+++ level-4/lvl4-cap-app/src/Jokecard.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
+//card styling: https://getbootstrap.com/docs/5.1/components/card/
+
 function Jokecard(props) {
 
+    const homeCardStyle = {
+        "width": "75%"
+    }
+    const libraryCardStyle = {
+        "width": "25%"
+    }
 
     return (
- 
-        <li>
-            <h3>{props.setup}</h3>
-            <h3>{props.joke}</h3>
-            <h6>{props.delivery}</h6>
+        // conditially render style based on which page is active
+        <li className="card" style={homeCardStyle}>
+            <div className="card-body">
+            <h3 className="card-title">{props.setup}</h3>
+            <h3 className="card-title">{props.joke}</h3>
+            <p className="card-text">{props.delivery}</p>
             {/* add categories and flags
             will need conditional rendering with boolean values in api data */}
+            </div>
         </li>
     )
 }
