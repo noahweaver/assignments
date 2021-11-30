e2302528f5467ba8634903d81ca61ed991a406d6
Noah Weaver <noahweaver@aol.com>
2021-11-30 07:55:56 -0600

    increased readability, fixed jokeslibrary routing, home link color so it doesn't appear active all the time.

--- level-4/lvl4-cap-app/src/reused_components/Nav.js
+++ level-4/lvl4-cap-app/src/reused_components/Nav.js
@@ -10,17 +10,28 @@ function Nav() {
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger py-4">
             {/* navbar-expand-sm: navbar turns vertical at 575px */}
             <div className="container-fluid">
-                <span className="navbar-brand">JokesApp</span>
+                <span className="navbar-brand">
+                    JokesApp
+                </span>
                 {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button> */}
                 <div className="navbar-collapse" id="navbarText navbarScroll">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-nav-scroll">
                         <li className="nav-item">
-                        <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
+                            <NavLink 
+                                className="nav-link" 
+                                aria-current="page" 
+                                to="/home">
+                                    Home
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                        <NavLink className="nav-link" to="/jokelibrary">Joke Library</NavLink>
+                            <NavLink 
+                                className="nav-link" 
+                                to="jokelibrary">
+                                    Joke Library
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
