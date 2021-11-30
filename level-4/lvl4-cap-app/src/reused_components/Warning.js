904eb421d8cc531474a3febc3e8900c77f1b0a21
Noah Weaver <noahweaver@aol.com>
2021-11-30 13:33:07 -0600

    warning for explicit content

---
+++ level-4/lvl4-cap-app/src/reused_components/Warning.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Warning() {
+    return (
+        <div className="alert alert-danger">
+            <p>WARNING: Proceed with caution. There are many dark and potentially offensive jokes used in this app. No offense is meant, but if you are easily offended you might not want to proceed.  </p>
+        </div>
+    )
+}
+
+export default Warning
