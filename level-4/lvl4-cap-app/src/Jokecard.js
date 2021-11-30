6959e971536f546c3bfb1a8feac7ced3fbd117e7
Noah Weaver <noahweaver@aol.com>
2021-11-30 08:49:27 -0600

    fixed to use props

--- level-4/lvl4-cap-app/src/Jokecard.js
+++ level-4/lvl4-cap-app/src/Jokecard.js
@@ -4,28 +4,21 @@ import {Context} from './Context'
 function Jokecard(props) {
 
     const {currentJoke, jokeList} = useContext(Context)
-    //home card style
-    //list card style
+        //home card style
+        //list card style
 
     return (
-        //if home page is active render single card, else render list format
-        <>
-        <div>
-            <h3>{currentJoke.setup}</h3>
-            <h3>{currentJoke.joke}</h3>
-            <h6>{currentJoke.delivery}</h6>
-        </div>
-{/* 
-        <div>
-            {props.setup}
-            {props.delivery}
-            {props.joke}
-        </div> */}
 
-        <div>
-            {jokeList}
-        </div>
-        </>
+        //conditional rendering
+            //if home page is active render single card, else render list format
+        //if I use props I may not need conditional rendering  
+        <li>
+            <h3>{props.setup}</h3>
+            <h3>{props.joke}</h3>
+            <h6>{props.delivery}</h6>
+            {/* add categories and flags
+            will need conditional rendering with boolean values in api data */}
+        </li>
     )
 }
 
