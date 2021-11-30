70ce3fb68d85dc1eb1885ca81e83795d66a847ab
Noah Weaver <noahweaver@aol.com>
2021-11-30 15:39:04 -0600

    changed name from sidenav, sidebar sticks to side, has submenus and some styling work done

---
+++ level-4/lvl4-cap-app/src/reused_components/Sidebar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function SideNav() {
+    
+    return (
+        <div className="col-lg-2"> 
+            <nav className="sidebar card py-4 px-3">
+                <span className="">Side Nav Title</span>
+                <ul className="nav flex-column" id="nav-accordian">
+                    <li className="nav-item">
+                        {/* change <a> to link or navlink */}
+                        <a className="nav-link" href="#">type 1</a>
+                    </li>
+                    <li className="nav-item has-submenu">
+                        {/* change <a> to link or navlink */}
+                        <a className="nav-link" href="#">submenu links</a>
+                        <ul className="submenu collapse">
+                            <li><a className="nav-link" href="#">submenu item 1</a></li>
+                            <li><a className="nav-link" href="#">submenu item 2</a></li>
+                            <li><a className="nav-link" href="#">submenu item 3</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        {/* change <a> to link or navlink */}
+                        <a className="nav-link" href="#">type 1</a>
+                    </li>
+                </ul>
+            </nav> 
+        </div>
+        
+    )
+}
+
+export default SideNav
