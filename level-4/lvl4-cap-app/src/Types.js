803f40336f5480996c088dfe101c18cef4ad330d
Noah Weaver <noahweaver@aol.com>
2021-11-30 15:41:05 -0600

    started building component

--- level-4/lvl4-cap-app/src/Types.js
+++ level-4/lvl4-cap-app/src/Types.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Nav from './reused_components/Nav'
 import Footer from './reused_components/Footer'
-import SideNav from './reused_components/SideNav'
+import Sidebar from './reused_components/Sidebar'
 
 function Types() {
     return (
         <>
             <Nav />
-            <SideNav />
-            <h1>Jokes by type   </h1>
+            <div className="section-content">
+                <div className="row">
+                    <Sidebar />
+                <div className="col-lg-9"> 
+                    <h1 >Jokes by type</h1>
+                    <ul>
+                        jokecards will render here
+                    </ul>                
+                </div>
+                </div>
+            </div>
+            
             <Footer />
         </>
     )
