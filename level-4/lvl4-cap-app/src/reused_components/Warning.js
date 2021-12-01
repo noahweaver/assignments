e103be927e92ce56fc5367a7b2c4ca9da1189e4e
Noah Weaver <noahweaver@aol.com>
2021-12-01 14:41:03 -0600

    bolded warning

--- level-4/lvl4-cap-app/src/reused_components/Warning.js
+++ level-4/lvl4-cap-app/src/reused_components/Warning.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Warning() {
     return (
         <div className="alert alert-danger position-absolute w-100">
-            <p>WARNING: Proceed with caution. There are many dark and potentially offensive jokes used in this app. No offense is meant, but if you are easily offended you might not want to proceed.  </p>
+            <p><b>WARNING:</b> Proceed with caution. There are many dark and potentially offensive jokes used in this app. No offense is meant, but if you are easily offended you might not want to proceed.  </p>
         </div>
     )
 }
