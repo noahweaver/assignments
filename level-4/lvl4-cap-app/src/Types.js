29d2af430efca754c173c1804bf52bdf45ab76e2
Noah Weaver <noahweaver@aol.com>
2021-12-01 17:24:42 -0600

    started work on jokes by types page

--- level-4/lvl4-cap-app/src/Types.js
+++ level-4/lvl4-cap-app/src/Types.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+// import {useParams} from 'react-router-dom'
+import {Context} from './Context'
 import Nav from './reused_components/Nav'
 import Footer from './reused_components/Footer'
 import Sidebar from './reused_components/Sidebar'
+import Jokecard from './Jokecard'
 
-function Types() {
+
+function Types(props) {
+    //probably going to pull jokesArr from context
+    const {} = useContext(Context)
+    // let params = useParams()
+
+    //function to render joke cards
+        //will need filter and/or map
+        //pass props
+
     return (
         <>
             <Nav />
@@ -14,6 +26,7 @@ function Types() {
                 <div className="col-lg-9"> 
                     <h1 >Jokes by type</h1>
                     <ul>
+                        {/* function doing rendering from above */}
                         jokecards will render here
                     </ul>                
                 </div>
