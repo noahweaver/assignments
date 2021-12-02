22b2a7845cceb17e931466c04c09865162323e34
Noah Weaver <noahweaver@aol.com>
2021-12-01 21:18:51 -0600

    finished adding checkboxes

--- level-4/lvl4-cap-app/src/reused_components/Sidebar.js
+++ level-4/lvl4-cap-app/src/reused_components/Sidebar.js
@@ -7,6 +7,16 @@ function Sidebar() {
     const [jokeForm, setJokeForm] = useState({
         xmas: false,
         dark: false,
+        misc: false,
+        programming: false,
+        pun: false,
+        spooky: false,
+        explicit: false,
+        nsfw: false,
+        political: false,
+        racist: false,
+        religious: false,
+        sexist: false,
     })
 
     function handleChange(event){
@@ -46,12 +56,78 @@ function Sidebar() {
                                     checked={jokeForm.dark}
                                     onChange={handleChange}
                                 /> Dark
+                                <input 
+                                    name="misc"
+                                    type="checkbox" 
+                                    checked={jokeForm.misc}
+                                    onChange={handleChange}
+                                /> Misc
+                                <input 
+                                    name="programming"
+                                    type="checkbox" 
+                                    checked={jokeForm.programming}
+                                    onChange={handleChange}
+                                /> Programming
+                                <input 
+                                    name="pun"
+                                    type="checkbox" 
+                                    checked={jokeForm.pun}
+                                    onChange={handleChange}
+                                /> Pun
+                                <input 
+                                    name="spooky"
+                                    type="checkbox" 
+                                    checked={jokeForm.spooky}
+                                    onChange={handleChange}
+                                /> Spooky
                             </li>
                         </ul>
                     </li>
-                    <li className="nav-item">
+                    <li className="nav-item has-submenu">
                         {/* change <a> to link or navlink */}
-                        <a className="nav-link" href="#">type 1</a>
+                        <a 
+                            className="nav-link" 
+                            to={"flag"}
+                            >Flag
+                        </a>
+                        <ul className="submenu collapse">
+                        <input 
+                            name="explicit"
+                            type="checkbox" 
+                            checked={jokeForm.explicit}
+                            onChange={handleChange}
+                        /> Explicit
+                        <input 
+                            name="nsfw"
+                            type="checkbox" 
+                            checked={jokeForm.nsfw}
+                            onChange={handleChange}
+                        /> NSFW
+                        <input 
+                            name="political"
+                            type="checkbox" 
+                            checked={jokeForm.political}
+                            onChange={handleChange}
+                        /> Political
+                        <input 
+                            name="racist"
+                            type="checkbox" 
+                            checked={jokeForm.racist}
+                            onChange={handleChange}
+                        /> Racist
+                        <input 
+                            name="religious"
+                            type="checkbox" 
+                            checked={jokeForm.religious}
+                            onChange={handleChange}
+                        /> Religious
+                        <input 
+                            name="sexist"
+                            type="checkbox" 
+                            checked={jokeForm.sexist}
+                            onChange={handleChange}
+                        /> Sexist
+                        </ul>
                     </li>
                 </ul>
                 </form>
