7cf58eda7359d0d43243753e8427a4f00b95c70f
Noah Weaver <noahweaver@aol.com>
2021-12-02 14:49:36 -0600

    deleted unused hooks

--- level-4/lvl4-cap-app/src/Library.js
+++ level-4/lvl4-cap-app/src/Library.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext, useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {Context} from './Context'
 import Jokecard from './Jokecard'
