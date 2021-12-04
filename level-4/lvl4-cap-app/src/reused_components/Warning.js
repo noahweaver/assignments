9ebb369f86f73ffca81a36f570fb199b8a6e2eb1
Noah Weaver <noahweaver@aol.com>
2021-12-04 08:30:05 -0600

    removed absolute positioning to make responsive

--- level-4/lvl4-cap-app/src/reused_components/Warning.js
+++ level-4/lvl4-cap-app/src/reused_components/Warning.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Warning() {
     return (
-        <div className="alert alert-danger position-absolute w-100">
+        <div className="alert alert-danger w-100">
             <p><b>WARNING:</b> Proceed with caution. There are many dark and potentially offensive jokes used in this app. No offense is meant, but if you are easily offended you might not want to proceed.  </p>
         </div>
     )
