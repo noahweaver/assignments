47cf2db16169112d661e96c530b1272b398fc4db
Noah Weaver <noahweaver@aol.com>
2021-12-06 15:03:31 -0600

    created math.js

---
+++ level-5/modules/math.js
@@ -0,0 +1,19 @@
+function sum(a, b){
+    return a + b
+}
+function subtact(a, b){
+    return a - b
+}
+function multiply(a, b){
+    return a * b
+}
+
+export const sum = sum
+export const subtact = subtact
+export const multiply = multiply
+//or
+// module.exports = {
+//     sum: sum,
+//     subtact: subtact,
+//     multiply: multiply
+// }
