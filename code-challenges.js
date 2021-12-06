b325dc081ea9993c181a79301e35d9ae07dfa4ce
Noah Weaver <noahweaver@aol.com>
2021-12-06 16:13:40 -0600

    code challenge 12/6/21

--- code-challenges.js
+++ code-challenges.js
@@ -173,3 +173,53 @@
 // }
 
 // console.log(array1)
+
+//12/6/21
+// Task
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+// The Haskell version takes a list of directions with data Direction = North | East | West | South.
+// The Clojure version returns nil when the path is reduced to nothing.
+// The Rust version takes a slice of enum Direction {North, East, West, South}.
+// function dirReduc(arr){
+    
+//   let noChanges = false
+  
+  
+//   while(!noChanges){
+//         console.log(arr)
+//     noChanges = true
+//     for(let i = 0; i < arr.length; i++){
+//       console.log(arr)
+//       if(arr[i] === "WEST"){
+//         if(arr[i+1] === "EAST"){
+//           arr.splice(i, 2)
+//           noChanges = false
+//         }
+//       }
+//       if(arr[i] === "EAST"){
+//         if(arr[i+1] === "WEST"){
+//           arr.splice(i, 2)
+//           noChanges = false
+
+//         }
+//       }
+//       if(arr[i] === "SOUTH"){
+//         if(arr[i+1] === "NORTH"){
+//           arr.splice(i, 2)
+//           noChanges = false
+
+//         }
+//       }
+//      if(arr[i] === "NORTH"){
+//         if(arr[i+1] === "SOUTH"){
+//           arr.splice(i, 2)
+//           noChanges = false
+
+//         }
+//       }
+//     }
+//   }
+  
+//   return arr
+// }
