d29280f99a70624aa65eee471f5a9f313ffea815
Noah Weaver <noahweaver@aol.com>
2021-12-08 10:11:29 -0600

    added bounties, put, and delete

--- level-5/bounty-hunter/routes/bountyRouter.js
+++ level-5/bounty-hunter/routes/bountyRouter.js
@@ -8,8 +8,24 @@ const bounties = [
         firstName: "Luke",
         lastName: "Skywalker",
         living: true,
+        bounty: 100000,
+        type: "jedi",
+        _id: uuid()
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        living: true,
         bounty: 10000000,
         type: "sith",
+        _id: uuid()
+    },
+    {
+        firstName: "Yo",
+        lastName: "da",
+        living: true,
+        bounty: 0,
+        type: "jedi",
         _id: uuid()
     }
 ]
@@ -25,8 +41,27 @@ bountyRouter.route("/")
         bounties.push(newBounty)
         res.send(`successfully added ${newBounty.firstName} ${newBounty.lastName} to the database`)
     })
-    // .put
-    // .delete
+
+bountyRouter.route("/:bountyId")
+    .get((req, res, next )=> {
+        const bountyId = req.params.bountyId
+        const filteredBounties = bounties.filter(bounty => bounty._id === bountyId)
+        res.send(filteredBounties)
+    })
+    .delete((req, res, next) => {
+        const bounty = req.body
+        const bountyId = req.params.bountyId
+        const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+        bounties.splice(bountyIndex, 1)
+        res.send(`successfully removed ${bounty.firstName} ${bounty.lastName} from the database`)
+    })
+    .put((req, res, next) => {
+        const updatedObject = req.body
+        const bountyId = req.params.bountyId
+        const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+        const updatedBounty = Object.assign(bounties[bountyIndex], updatedObject)
+        res.send(updatedBounty)
+    })
 
 
 
