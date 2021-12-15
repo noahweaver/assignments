d60b073d5c065a7186ebaa3f4ec13229ad4abfe8
Noah Weaver <noahweaver@aol.com>
2021-12-15 15:17:01 -0600

    code challenge 12/15/21

--- code-challenges.js
+++ code-challenges.js
@@ -293,3 +293,18 @@
 //     }
 //   }
 // }
+//12/15/2021
+// const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24
+
+// function getDaysBetweenDates(dateText1, dateText2) {
+//   const date1 = new Date(dateText1)
+//   const date2 = new Date(dateText2)
+
+//   const diffTime = Math.abs(date2-date1)
+//   const diffDays = Math.ceil(diffTime/ DAY_IN_MILLISECONDS)
+
+//   return diffDays
+
+// }
+
+// console.log(getDaysBetweenDates('10/15/2020', '12/1/2020'));
